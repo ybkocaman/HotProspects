7fd86c44dd27d75f0566a90dc2a19662d8d98fe0
Yusuf <ybkocaman@gmail.com>
2024-01-09 07:39:53 +0000

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Yusuf Burak on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
